91f6f432ff348505f7e81ee6acdce19eb18e7352
65050701-hue <65050701@kmitl.ac.th>
2025-09-18 18:23:33 +0700

    ex11

--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -3,7 +3,7 @@ import sys
 params = sys.argv[1:]
 
 if params:
-    print("parameters:")
+    print("parameters:",len(params))
     for param in params:
         print(f"{param}: {len(param)}")
 else:
